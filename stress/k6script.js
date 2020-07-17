f1a2e87a20b2f9ab83e2beb65b0ec7b18d2320e7
robinnnnnn <rlifshitz19@gmail.com>
2020-07-16 17:15:55 -0700

    Basic Stress Testing @ 400 RPS working

--- stress/k6script.js
+++ stress/k6script.js
@@ -2,17 +2,17 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 
 export const options = {
-  vus: 300,
-  duration: '1s',
+  vus: 200,
+  duration: '10s',
 
 };
 
 export default function () {
-  const res = http.get('http://localhost:3000/songs');
+  const res = http.get('http://localhost:3273/songs');
 
   check(res, {
     'is status 200': (r) => r.status === 200,
   });
 
-  sleep(1);
+  sleep(.5);
 }
