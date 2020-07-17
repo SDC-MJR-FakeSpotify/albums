620b14c8baa1648c31801eac00a0d922082f5605
robinnnnnn <rlifshitz19@gmail.com>
2020-07-17 14:27:22 -0700

    Ignore License Files

--- database/dbSDC.js
+++ database/dbSDC.js
@@ -1,6 +1,6 @@
 const { Sequelize, DataTypes } = require('Sequelize');
 
-// const sequelize = new Sequelize('postgres://localhost:5432/sdc');
+// const sequelize = new Sequelize('postgres://localhost:5432/sdc'); Add dns to instance
 const sequelize = new Sequelize('sdc', 'robinlifshitz', '', {
   dialect: 'postgres',
 });
