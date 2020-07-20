a74308e7f96e52a2ff390a2484c76230b45c67b3
robinnnnnn <rlifshitz19@gmail.com>
2020-07-20 15:16:11 -0700

    Joey and Marek Rock

--- database/dbSDC.js
+++ database/dbSDC.js
@@ -1,7 +1,7 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
 // const sequelize = new Sequelize('postgres://localhost:5432/sdc'); Add dns to instance
-const sequelize = new Sequelize('sdc', 'robinlifshitz', '', {
+const sequelize = new Sequelize('sdc', 'postgres', 'robin', {
   host: '50.18.23.146',
   dialect: 'postgres',
 });
